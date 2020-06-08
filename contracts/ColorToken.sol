2b058eb68af86acf26c671e6b688ea8e208de82a
AditiSharma85 <59632607+AditiSharma85@users.noreply.github.com>
2020-06-08 00:23:39 -0400

    added basic test for token

--- contracts/ColorToken.sol
+++ contracts/ColorToken.sol
@@ -1,11 +1,24 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.0;
-//pragma experimental ABIEncoderV2;
+pragma experimental ABIEncoderV2;
 
 import "./ERC721.sol";
 
 contract ColorToken is ERC721 {
-	constructor () ERC721("ColorToken","CT") public {
+	string[] public colors;
+mapping(string=>bool) _colorExists;
+constructor () ERC721("ColorToken","CT") public {
+	}
+	// E.G. color="#FFFFFF"
+	function mint(string memory _color) public {
+		//require unique color
+		require(_colorExists[_color]==false,"Color token already exists");
+		colors.push(_color);
+		uint _id = colors.length - 1;
+		_mint(msg.sender, _id);
+		_colorExists[_color] = true;
+		//Call the mint function
+		//Track it
 	}
 }
--- test/Color.test.js
+++ test/Color.test.js
@@ -1,4 +1,4 @@
-const ColorToken = artifacts.require('./Color.sol');
+const ColorToken = artifacts.require('./ColorToken.sol');
 require('chai')
 	.use(require('chai-as-promised'))
 	.should()
@@ -13,7 +13,7 @@ contract('ColorToken',(accounts) => {
 
 describe('deployment',async() => {
 	it('deploys successfully', async () => {
-		contract = await FifaToken.deployed();
+		contract = await ColorToken.deployed();
 		const address = contract.address;
 		console.log(address);
 		assert.notEqual(address, '');
@@ -30,6 +30,19 @@ describe('deployment',async() => {
 	it('has a symbol', async () => {
 		const symbol = await contract.symbol();
 		assert.equal(symbol, 'CT');
-	})
+    })
+    describe('minting', async() => {
+        it('creates a new token', async () => {
+            const checkBalance = await contract.balanceOf.call(accounts[0]);
+            assert.equal(checkBalance,0,'Account balance is zero');
+            const result = await contract.mint('#EC0588');
+            const totalSupply = await contract.totalSupply();
+            //SUCCESS
+            assert.equal(totalSupply,1);
+            console.log(result)
+                       
+        })
+    })
+    
 })
 })
